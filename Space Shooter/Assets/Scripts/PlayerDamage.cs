9e0385224d8a85f90a3d4f854b6a5b7e81c680a4
StevenEnt <stenterline@gmail.com>
2020-04-12 23:41:10 -0400

    Put in the health bar and updated the engery bar

---
+++ Space Shooter/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : Damage
+{
+     [SerializeField] private HealthBar HpBar; 
+     float healthPercent;
+
+     void Start(){
+         healthPercent = 1;
+     }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isInvincible == false){
+            hp--;
+            healthPercent -= .25f;
+            HpBar.updateEnergy(healthPercent);
+            invincibility();
+        }
+        
+        if (hp == 0)
+        {
+            HpBar.updateEnergy(0);
+            Die();
+        }
+    }
+}
